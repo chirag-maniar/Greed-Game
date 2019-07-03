3633fe1cd80887b2d2d95b39c672048aa51d9bb2
Chirag Maniar <chiragmaniar@Chirags-MacBook-Pro.local>
2019-07-03 20:55:52 +0530

    added solution to greed game

---
+++ dice_set.rb
@@ -0,0 +1,15 @@
+
+class DiceSet
+
+  attr_reader :values
+  def roll(num)
+    
+    @values = []
+    i=0
+    while i<num
+      @values << rand(1..6)
+      i += 1
+    end
+    @values
+  end
+end
---
+++ player.rb
@@ -0,0 +1,160 @@
+require './dice_set'
+
+class Player 
+	attr_accessor :player
+	def initialize(name)
+		@player = {
+			name: name,
+			non_scoring: 5,
+			temp_score: 0,
+			fin_score: 0 
+			
+		}
+		@player
+	end
+
+	def score(dice)
+  		
+  	  	
+	  count = Hash.new(0) 
+	  result = 0
+	  dice.each { |val|
+	    count[val] += 1
+	  }
+	  
+	  count.each { |item, times|
+
+	    if item == 1 && times >= 3 then
+	      result += 1000
+	      times -= 3
+	      @player[:non_scoring] -= 3
+	    end
+
+	    if item == 5 && times >= 3 then 
+	      result += 500
+	      times -= 3
+	      @player[:non_scoring] -= 3
+	    end
+	    
+	    if item == 5 && times <=2 then
+	      result += 50 * times
+	      @player[:non_scoring] -= times
+	    end
+
+	    if item != 1 && times >= 3 then
+	      result += item * 100
+	      times -= 3
+	      @player[:non_scoring] -= times
+	    end
+	    
+	    if item == 1 && times <= 2 then 
+	      result += 100 * times
+	      @player[:non_scoring] -= times
+	    end
+
+	  }
+  
+  	return result
+	end
+
+	def roll_dice(player) 
+		temp = []
+	  	diceset_obj = DiceSet.new
+		temp = diceset_obj.roll(player[:non_scoring])
+		puts "Player #{player[:name]+1} rolls: #{temp}"
+		score(temp) 
+	end
+
+end
+
+class Game
+	def initialize()
+		@players = []
+		@last = {} 
+		@last_player = 0 
+		@rnd = 1 
+	end
+
+
+	def calculate(score, player) 
+		i = player[:name].next
+		puts "Score in this round: #{score}"
+		puts "Total score: #{player[:fin_score]}"
+		if (player[:temp_score] == 0  && score >= 0) || player[:temp_score] != 0
+			if score != 0
+				player[:temp_score] += score
+			  	puts "Press 1 if you wish to roll the non-scoring #{player[:non_scoring]} dices. Else press 0 :"
+				c = gets.chomp
+
+				if c == "1"
+					if player[:temp_score] > 0
+					  	i = player[:name] 
+					end
+				elsif c == "0"
+					puts "\n"
+					if player[:temp_score] >= 300 || player[:fin_score] >= 300
+						player[:fin_score] += player[:temp_score] 
+					end
+
+					if player[:fin_score] >= 3000 && !@last[:flag]
+						puts "Last turn"
+						@last = {flag: true, end: i == @players.length}
+					end
+				end
+			end
+		end
+		if i == player[:name].next
+			again(player)
+		end
+		play(i)
+	end
+
+	def again(player)
+		player[:non_scoring] = 5 
+		player[:temp_score] = 0 
+	end
+
+	def initial_start 
+		puts "\nEnter the number of players to play:"
+		no = gets.chomp
+		num_players = no.to_i
+		num_players.times { |player|
+			@players.push Player.new(player)
+		}
+		puts "\nTurn 1: \n"
+		play(0) 
+	end
+
+	def play(i)
+		if @last[:end] && i == @players.length 
+			i = 0 
+			@last_player = 1 
+			if @last[:flag]
+				@last[:end] = false 
+			end
+		end
+		if i < @players.length-@last_player
+			t = @players[i]
+			s = t.player
+			score = t.roll_dice(s) 
+
+			calculate(score, s)
+		else
+			@rnd += 1
+			puts "Turn #{@rnd}: \n" if !@last[:flag]
+			if !@last[:flag] || @last[:end] 
+				play(0) 
+			else
+				winner = @players.max_by { |player|
+				 	player.player[:fin_score]
+				}
+				puts "\nPlayer #{winner.player[:name]+1} won. Final score is #{winner.player[:fin_score]} \n"
+			end
+		end
+	end 
+end
+
+g = Game.new
+g.initial_start 
+
+
